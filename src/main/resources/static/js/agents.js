ef3879ee158a33d01a9e43caf8251467d7143d6a
steve <vyacheslav.danovich@mastercard.com>
2019-09-10 22:19:18 -0400

    @BPS-519 added agent list

--- src/main/resources/static/js/agents.js
+++ src/main/resources/static/js/agents.js
@@ -1,48 +1,12 @@
-Vue.component('htmlmodal', {
-	template: '#html-modal-template',
-	data() {
-       return {
-         options: null,
-         agent: null
-       };
-    },
-    created() {
-      this.getOptions();
-    },
-    methods: {
-      getOptions() {
-        axios.get('/bps/discovery/v0.1/discovery/agents')
-            .then(response => {this.options = response.data}).catch(function(error) {
-                console.log('an error occurred ' + error);
-            });
-      },
-      onChange:function(event){
-
-       console.log('Selected Agent '+event.target.options[event.target.options.selectedIndex].text);
-       this.agent = event.target.options[event.target.options.selectedIndex].text;
-        //Store in global var
-       store.state.message = this.agent;
-      }
-    },
-
-	});
-
-	// START THE Buyer Registration
-	new Vue({
-		el:'#button-container1',
-		data: {
-			showModal: false
-		},
-        methods: {
-            onLoad(e) {
-            }
+axios.get("/bps/discovery/v0.1/discovery/agents")
+    .then((results) => {
+        var html = '<option disabled="disabled" selected="selected" >-- Select Agent --</option>';
+        var len = results.data.length;
+        for ( var i = 0; i < len; i++) {
+            html += '<option value="' + results.data[i] + '">'
+                + results.data[i] + '</option>';
         }
-	});
-
-	// START THE Supplier Registration
-	new Vue({
-		el:'#button-container2',
-		data: {
-			showModal: false
-		}
-	});
+        html += '</option>';
+        var agentName = document.getElementById('agentName');
+        agentName.innerHTML = html;
+    });
